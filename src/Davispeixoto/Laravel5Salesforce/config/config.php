d217b2f6d500aca420b3908bd5d7ba8b075c6656
kimdelmote <kimdelmote3@hotmail.com>
2016-12-12 08:11:10 +0100

    Allow Soap cache option in config

--- src/Davispeixoto/Laravel5Salesforce/config/config.php
+++ src/Davispeixoto/Laravel5Salesforce/config/config.php
@@ -1,5 +1,6 @@
 <?php
-return [
+
+$config = [
 
     /*
     |--------------------------------------------------------------------------
@@ -10,8 +11,15 @@ return [
     */
 
     // production
-    'username' => env('SALESFORCE_USERNAME'),
-    'password' => env('SALESFORCE_PASSWORD'),
-    'token' => env('SALESFORCE_TOKEN'),
-    'wsdl' => storage_path('app/' . env('SALESFORCE_WSDL', 'wsdl/enterprise.wsdl.xml'))
+    'username'     => env('SALESFORCE_USERNAME'),
+    'password'     => env('SALESFORCE_PASSWORD'),
+    'token'        => env('SALESFORCE_TOKEN'),
+    'wsdl'         => storage_path('app/' . env('SALESFORCE_WSDL', 'wsdl/enterprise.wsdl.xml')),
+    'soap_options' => [],
 ];
+
+if (env('SALESFORCE_CACHE_NONE', false)) {
+    $config['soap_options']['cache_wsdl'] = WSDL_CACHE_NONE;
+}
+
+return $config;
